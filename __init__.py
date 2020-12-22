4d69a2e9725d84307c65fd4e54f09ce88f214acf
Luca Krämer <lk152@hdm-stuttgart.de>
2020-12-22 17:08:11 +0100

    created class

--- __init__.py
+++ __init__.py
@@ -1,12 +1,17 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from caldav.examples.nextcloud_starter import Calendar
 
 class ManageAppointments(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        calendar = Calendar()
+        print("Next Appointment: " + calendar.getEventName())
+
+        
 
     @intent_file_handler('appointments.manage.intent')
     def handle_appointments_manage(self, message):
+        print(message)
         self.speak_dialog('appointments.manage')
 
 
