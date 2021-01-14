ddfc26596d6c11d961090e6156ef2a9250fac945
Luca Krämer <lk152@hdm-stuttgart.de>
2021-01-14 17:51:11 +0100

    test

--- __init__.py
+++ __init__.py
@@ -17,7 +17,8 @@ class ManageAppointments(MycroftSkill):
         day = message.data.get('day')
         month = message.data.get('month')
         if (day is not None and month is not None):
-            self.speak_dialog("On the " + day + " of " + month + " you have the following appointments")
+            self.loadCalendars()
+            self.speak_dialog(self.getAppointmentsOnDate(day, month))
         else:
             self.speak_dialog("hello")
         
@@ -91,6 +92,8 @@ class ManageAppointments(MycroftSkill):
             start=datetime.today(), end=datetime(2024, 1, 1), expand=True)
         
         
+        
+        
         if len(events_fetched)!=0:
             next_appointment = events_fetched.pop()
             self.nextEvent= True
@@ -120,6 +123,69 @@ class ManageAppointments(MycroftSkill):
             return "There are no upcoming events"
         else:
             return "Your next appointment is on " + self.appointment_date + self.appointment_time + " and is entitled " + self.appointent_name
+        
+    def getAppointmentsOnDate(self,day,month):
+        year = datetime.date.today().year
+            
+        with open('pw.txt','r') as file:
+            data = file.read().splitlines()
+        
+        username = data[0]
+        password = data[1]
+
+        
+        url = "https://" + username + ":" + password + "@next.social-robot.info/nc/remote.php/dav"
+        
+        #vcal = """BEGIN:VCALENDAR
+        #VERSION:2.0
+        #PRODID:-//Example Corp.//CalDAV Client//EN
+        #BEGIN:VEVENT
+        #UID:1234567890
+        #DTSTAMP:20100510T182145Z
+        #DTSTART:20100512T170000Z
+        #DTEND:20100512T180000Z
+        #SUMMARY:This is an event
+        #END:VEVENT
+        #END:VCALENDAR
+        #"""
+        
+        # example event object
+        #<VCALENDAR| [<VERSION{}2.0>, <CALSCALE{}GREGORIAN>, 
+        #             <PRODID{}-//Sabre//Sabre VObject 4.1.6//EN>, 
+        #             <VEVENT| [<UID{}e8e55e25-1d7c-4bc5-a115-29480d076a30>, 
+        #                       <DTSTART{}2020-02-17 09:30:00+00:00>, 
+        #                       <DTEND{}2020-02-17 10:50:00+00:00>, 
+        #                       <CREATED{}2020-02-16 19:53:30+00:00>, 
+        #                       <DTSTAMP{}2020-02-16 19:54:08+00:00>, 
+        #                       <LAST-MODIFIED{}2020-02-16 19:54:08+00:00>, 
+        #                       <SEQUENCE{}2>, 
+        #                       <SUMMARY{}Testtermin>]>]>
+        
+        # open connection to calendar
+        client = caldav.DAVClient(url)
+        principal = client.principal()
+        # get all available calendars (for this user)
+        calendars = principal.calendars()
+        if calendars:
+            ## Some calendar servers will include all calendars you have
+            ## access to in this list, and not only the calendars owned by
+            ## this principal.  TODO: see if it's possible to find a list of
+            ## calendars one has access to.
+            print("your principal has %i calendars:" % len(calendars))
+            for c in calendars:
+                calendar_url = c.url
+                print("Name: %-20s  URL: %s" % (c.name, c.url))
+        else:
+            print("your principal has no calendars")
+            
+        events_fetched = calendars.date_search(
+            start=datetime(year, month, day), end=datetime(year, month, day), expand=True)
+        
+        next_appointment = events_fetched.pop()
+        event = next_appointment.instance.vevent
+        appointent_name = event.summary.value
+        return "On the " + day + " of " + month + " you have the following appointments: " + appointent_name
+        
 
 
 def create_skill():
