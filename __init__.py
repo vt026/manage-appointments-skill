59a64969d4f399ba87c1896f30bfd0b4f4e1c8fd
Vinc PC <vt026@hdm-stuttgart.de>
2021-01-15 10:58:26 +0100

    update getUsername

--- __init__.py
+++ __init__.py
@@ -31,42 +31,11 @@ class ManageAppointments(MycroftSkill):
     def loadCalendars(self):
         # Caldav url
         # import secret login code from local file here
-        
-        with open('pw.txt','r') as file:
-            data = file.read().splitlines()
-        
-        username = data[0]
-        password = data[1]
+    
 
-        
         url = "https://" + self.getUsername() + ":" + self.getPassword() + "@next.social-robot.info/nc/remote.php/dav"
         
-        #vcal = """BEGIN:VCALENDAR
-        #VERSION:2.0
-        #PRODID:-//Example Corp.//CalDAV Client//EN
-        #BEGIN:VEVENT
-        #UID:1234567890
-        #DTSTAMP:20100510T182145Z
-        #DTSTART:20100512T170000Z
-        #DTEND:20100512T180000Z
-        #SUMMARY:This is an event
-        #END:VEVENT
-        #END:VCALENDAR
-        #"""
-        
-        # example event object
-        #<VCALENDAR| [<VERSION{}2.0>, <CALSCALE{}GREGORIAN>, 
-        #             <PRODID{}-//Sabre//Sabre VObject 4.1.6//EN>, 
-        #             <VEVENT| [<UID{}e8e55e25-1d7c-4bc5-a115-29480d076a30>, 
-        #                       <DTSTART{}2020-02-17 09:30:00+00:00>, 
-        #                       <DTEND{}2020-02-17 10:50:00+00:00>, 
-        #                       <CREATED{}2020-02-16 19:53:30+00:00>, 
-        #                       <DTSTAMP{}2020-02-16 19:54:08+00:00>, 
-        #                       <LAST-MODIFIED{}2020-02-16 19:54:08+00:00>, 
-        #                       <SEQUENCE{}2>, 
-        #                       <SUMMARY{}Testtermin>]>]>
-        
-        # open connection to calendar
+
         client = caldav.DAVClient(url)
         principal = client.principal()
         # get all available calendars (for this user)
@@ -124,37 +93,11 @@ class ManageAppointments(MycroftSkill):
             return "Your next appointment is on " + self.appointment_date + self.appointment_time + " and is entitled " + self.appointent_name
         
     def getAppointmentsOnDate(self,day,month):
-        year = 2021
-            
-        
+        year = datetime.today().year
+                
         url = "https://" + self.getUsername() + ":" + self.getPassword() + "@next.social-robot.info/nc/remote.php/dav"
         
-        #vcal = """BEGIN:VCALENDAR
-        #VERSION:2.0
-        #PRODID:-//Example Corp.//CalDAV Client//EN
-        #BEGIN:VEVENT
-        #UID:1234567890
-        #DTSTAMP:20100510T182145Z
-        #DTSTART:20100512T170000Z
-        #DTEND:20100512T180000Z
-        #SUMMARY:This is an event
-        #END:VEVENT
-        #END:VCALENDAR
-        #"""
-        
-        # example event object
-        #<VCALENDAR| [<VERSION{}2.0>, <CALSCALE{}GREGORIAN>, 
-        #             <PRODID{}-//Sabre//Sabre VObject 4.1.6//EN>, 
-        #             <VEVENT| [<UID{}e8e55e25-1d7c-4bc5-a115-29480d076a30>, 
-        #                       <DTSTART{}2020-02-17 09:30:00+00:00>, 
-        #                       <DTEND{}2020-02-17 10:50:00+00:00>, 
-        #                       <CREATED{}2020-02-16 19:53:30+00:00>, 
-        #                       <DTSTAMP{}2020-02-16 19:54:08+00:00>, 
-        #                       <LAST-MODIFIED{}2020-02-16 19:54:08+00:00>, 
-        #                       <SEQUENCE{}2>, 
-        #                       <SUMMARY{}Testtermin>]>]>
-        
-        # open connection to calendar
+
         client = caldav.DAVClient(url)
         principal = client.principal()
         # get all available calendars (for this user)
